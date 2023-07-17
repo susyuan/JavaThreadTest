0da2ce1f3c68ce015805675e4ab284da46282dd0
susyuan <2027676594@qq.com>
2023-07-17 20:42:02 +0800

    修改线程2

--- src/test/java/com/duoduo/demo/Demo2.java
+++ src/test/java/com/duoduo/demo/Demo2.java
@@ -3,4 +3,15 @@ package com.duoduo.demo;
  创建线程方式2：实现Runnable接口
  */
 public class Demo2 {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new MyThread1());
+        thread1.setName("线程1");
+        thread1.start();
+    }
+}
+class MyThread1 implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
 }
