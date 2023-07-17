99c35111e1b6af5fdd65f7d3c1452279348a49b5
susyuan <2027676594@qq.com>
2023-07-17 20:37:58 +0800

    添加 线程创建方式

---
+++ src/test/java/com/duoduo/demo/Demo1.java
@@ -0,0 +1,27 @@
+package com.duoduo.demo;
+/*
+    创建线程方式1 继承Thread类
+    线程1 2 出现的顺序不一定
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        MyThread thread1 = new MyThread("线程1");
+        MyThread thread2 = new MyThread();
+        thread1.start();
+        thread2.setName("线程2");
+        thread2.start();
+    }
+}
+
+class MyThread extends  Thread {
+    MyThread (String name){
+        super(name);
+    }
+    MyThread ( ){
+
+    }
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
---
+++ src/test/java/com/duoduo/demo/Demo2.java
@@ -0,0 +1,6 @@
+package com.duoduo.demo;
+/*
+ 创建线程方式2：实现Runnable接口
+ */
+public class Demo2 {
+}
